257baca59507b68f8904947d1b0ba4594d88aeac
mlopez <lopezmatthew87@gmail.com>
2021-05-25 21:57:17 -0400

    updating js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,8 @@
-var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=518cefa1a9edeefef3077a2f5077d0a7"
-var forcastURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=518cefa1a9edeefef3077a2f5077d0a7"
+var lon = 0;
+var lat = 0;
+
+var forcastURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&appid=518cefa1a9edeefef3077a2f5077d0a7";
+
 
 
 
@@ -7,16 +10,59 @@ var getWeather = function (user) {
     // format the weather api url
     
 
-    fetch(weatherURL).then(function(response) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+user+"&appid=2fd9f6120b1e5e49f6b0893e50ef57f6").then(function(response) {
         response.json().then(function (data) {
-            console.log(data);
+         lon = data.coord.lon;
+         lat = data.coord.lat;
+         
+         displayWeather(lat, lon);
+
+         
+
         });
     });
 };
 
-// $("#cityBtn").on("click"), function() {
-//     $(this).
-// };
+var displayWeather = function (lat, lon) {
+    fetch(forcastURL).then(function(response) {
+        response.json().then(function (data) {
+            console.log(data);
+
+            renderWeather(data);
+        })
+    })
+};
+
+var renderWeather = function (data) {
+    temp = data.current.temp+" °F";
+    humidity = data.current.humidity+" %";
+    uvi = data.current.uvi;
+    wind = data.current.wind_speed+" mph";
+
+    $("#temp").append(temp);
+    $("#uvi").append(uvi);
+    $("#humidity").append(humidity);
+    $("#wind").append(wind);
+
+
+}
+
+ $("#cityBtn").on("click", function() {
+     alert("button was clicked");
+//    var buttonData =  $("#entercity").val();
+//    console.log(buttonData);
+
+   getWeather(buttonData);
+
+});
+
+
+
+
+
+
+
+
 
 
 // var formEl = document.querySelector("#user-form");
