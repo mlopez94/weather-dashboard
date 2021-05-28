33380b60b568fa09e7f9999bfa7445dc47b3709b
mlopez <lopezmatthew87@gmail.com>
2021-05-27 21:21:08 -0400

    cleared out on search and testing out data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,89 +1,83 @@
 var lon = 0;
 var lat = 0;
 
-
-
-
-
-
 var getWeather = function (user) {
-    // format the weather api url
-    
+  // format the weather api url
 
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+user+"&appid=2fd9f6120b1e5e49f6b0893e50ef57f6").then(function(response) {
-        response.json().then(function (data) {
-         lon = data.coord.lon;
-         lat = data.coord.lat;
-         
-         displayWeather(lat, lon);
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      user +
+      "&appid=2fd9f6120b1e5e49f6b0893e50ef57f6"
+  ).then(function (response) {
+    response.json().then(function (data) {
+      lon = data.coord.lon;
+      lat = data.coord.lat;
 
-         
-
-        });
+      displayWeather(lat, lon);
     });
+  });
 };
 
 var displayWeather = function (lat, lon) {
-    var forcastURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&appid=518cefa1a9edeefef3077a2f5077d0a7";
-    fetch(forcastURL).then(function(response) {
-        response.json().then(function (data) {
-            console.log(data);
+  var forcastURL =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&units=imperial&appid=518cefa1a9edeefef3077a2f5077d0a7";
+  fetch(forcastURL).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
 
-            renderWeather(data);
-        })
-    })
+      renderWeather(data);
+    });
+  });
 };
 
 var renderWeather = function (data) {
-    temp = data.current.temp+" °F";
-    humidity = data.current.humidity+" %";
-    uvi = data.current.uvi;
-    wind = data.current.wind_speed+" mph";
+  $("#temp").empty();
+  $("#uvi").empty();
+  $("#humidity").empty();
+  $("#wind").empty();
 
-    $("#temp").append(temp);
-    $("#uvi").append(uvi);
-    $("#humidity").append(humidity);
-    $("#wind").append(wind);
+  temp = data.current.temp + " °F";
+  humidity = data.current.humidity + " %";
+  uvi = data.current.uvi;
+  wind = data.current.wind_speed + " mph";
+
+  $("#temp").append(temp);
+  $("#uvi").append(uvi);
+  $("#humidity").append(humidity);
+  $("#wind").append(wind);
+
+  for (i=1; i < 6; i++) {
+    var temp = data.daily[i].temp.day;
+    var uvi = data.daily[i].uvi;
+    var humidity = data.daily[i].humidity;
+    var wind = data.daily[i].wind_speed;
+
+    $
+
+    console.log(temp);
 
 
-}
-
- $("#cityBtn").click (function(event) {
-    event.preventDefault();
-    var buttonData =  $("#entercity").val();
 
 
-   getWeather(buttonData);
 
+
+
+  
+
+
+
+
+  }
+};
+
+$("#cityBtn").click(function (event) {
+  event.preventDefault();
+  var buttonData = $("#entercity").val();
+
+  getWeather(buttonData);
 });
 
-
-
-
-
-
-
-
-
-
-// var formEl = document.querySelector("#user-form");
-// var inputEl = document.querySelector("#entercity");
-
-
-// // testing button
-// var formSubmitHandler = function (event) {
-//     event.preventDefault();
-//     //get value from input element
-//     var entercity = inputEl.value.trim();
-
-//     if(entercity) {
-//         getWeather(entercity);
-//         inputEl.value = "";
-//     } else {
-//         alert("Please enter a city");
-//     }
-
-//     console.log(event);
-// };
-
-// formEl.addEventListener("submit", formSubmitHandler);
